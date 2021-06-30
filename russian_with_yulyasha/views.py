feb594b72500df3cdbef2c8dbc7a405587ca48f7
StefSmlk <gsstteeff7777@gmail.com>
2021-06-30 18:32:18 +0300

    i did news list and enlargement of images

--- russian_with_yulyasha/views.py
+++ russian_with_yulyasha/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
+import requests
+from lxml import etree
 
 
 def home_view(request):
-    return render(request, 'home.html', {})
+    resp = requests.get('https://www.themoscowtimes.com/rss/news')
+    with open('my.xml', 'w', encoding='UTF-8') as my:
+        my.write(resp.text)
+
+    with open('my.xml', 'rb') as fobj:
+        xml = fobj.read()
+
+    list_of_news = []
+
+    root = etree.fromstring(xml)
+    news = root.getchildren()[0].getchildren()
+    for j in range(7, 13):
+        temp_list_of_news = []
+        for i in range(len(news[j].getchildren())):
+            if i < 4:
+                temp_list_of_news.append(news[j].getchildren()[i].text)
+        list_of_news.append(temp_list_of_news)
+
+    return render(request, 'home.html', {'news': list_of_news})
